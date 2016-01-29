afd1cd1e7c3fde3eaf7dcb99fc6734e0cb941487
richarddewit <henk.exe@gmail.com>
2016-01-29 08:10:08 +0100

    Add mkcd alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,9 @@ alias ae="$EDITOR $DOTFILES/zsh/aliases.zsh"
 alias ar="source $DOTFILES/zsh/aliases.zsh"
 alias ze="$EDITOR $DOTFILES/zsh/.zshrc"
 alias zr="source $DOTFILES/zsh/.zshrc"
+
+function mkcd
+{
+  command mkdir $1 && cd $1
+}
+alias mkcd=mkcd
