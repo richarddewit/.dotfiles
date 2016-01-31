7c77f5cfdb65bb28d7db01a934ad32dbc427317e
Richard de Wit <henk.exe@gmail.com>
2016-01-31 12:16:47 +0100

    Add some more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,12 @@
+# Default alias 'ag' is bound to 'sudo apt-get'..
+if [ -f /usr/bin/ag ]; then
+  # Use silversurfer-ag instead
+  alias ag="/usr/bin/ag"
+fi
+
 # ZSH
+alias "$"=""
+alias dotfiles="$EDITOR $DOTFILES"
 alias ae="$EDITOR $DOTFILES/zsh/aliases.zsh"
 alias ar="source $DOTFILES/zsh/aliases.zsh"
 alias ze="$EDITOR $DOTFILES/zsh/.zshrc"
@@ -9,3 +17,6 @@ function mkcd
   command mkdir $1 && cd $1
 }
 alias mkcd=mkcd
+
+# Mercurial
+alias hgss='hg status -S'
