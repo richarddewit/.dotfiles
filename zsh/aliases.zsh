8eb9522feb8c07f9ba7c609109cc13ddccfb73fd
Richard de Wit <henk.exe@gmail.com>
2016-02-25 23:07:12 +0100

    Add additional aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,11 +13,39 @@ alias ze="$EDITOR $DOTFILES/zsh/.zshrc"
 alias zr="source $DOTFILES/zsh/.zshrc"
 alias sr="exec $SHELL"
 
+alias ll='ls -alh --color=auto'
+alias ls='ls -lh --color=auto'
+
 function mkcd
 {
   command mkdir -p $1 && cd $1
 }
 alias mkcd=mkcd
 
+# FASD
+alias a='fasd -a' # any
+alias s='fasd -si' # show / search / select
+alias d='fasd -d' # directory
+alias f='fasd -f' # file
+alias z='fasd_cd -d' # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # interactive directory jump
+
 # Mercurial
 alias hgss='hg status -S'
+
+# React Native
+alias rns='react-native start'
+alias rna='react-native run-android'
+
+# NPM
+alias npmg='npm i -g'
+alias npms='npm i -S'
+alias npmd='npm i -D'
+
+# Vagrant
+alias vsus='vagrant suspend'
+alias vssh='vagrant ssh'
+alias vup='vagrant up'
+
+# TortoiseHG
+alias thg='thg &>/dev/null'
