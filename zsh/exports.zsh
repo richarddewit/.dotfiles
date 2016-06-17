d059921cbd01362b856f6638ffa28d9b63285f70
Richard de Wit <henk.exe@gmail.com>
2016-06-17 15:54:55 +0200

    Fix ~ or hard coded path to $HOME and change default editor

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,9 +1,9 @@
 # Path to your oh-my-zsh installation.
-export ZSH=/home/richard/.oh-my-zsh
+export ZSH="$HOME/.oh-my-zsh"
 
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
 
-export EDITOR="gmacs"
+export EDITOR="nvim"
 
 export NVM_DIR="$HOME/.nvm"
